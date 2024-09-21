7f781dcd3c251eae4fc39c6208104d99eb964bcb
wiji <wijigaming.yt@gmail.com>
2024-09-20 17:30:06 -0700

    added aspect endpoint

--- src/endpoints/report-aspect-endpoint.js
+++ src/endpoints/report-aspect-endpoint.js
@@ -4,12 +4,17 @@ const {getPlayerUUID, insertAspect} = require("../database");
 const {requestUUID} = require("../misc");
 
 class ReportAspectEndpoint {
+    constructor() {
+        this.recentReporters = new Set();
+    }
 
     async call(req, res) {
         let token = req.query.token;
         let {giver, receiver, reporter} = req.query;
 
         if (!token || !giver || !receiver) return res.status(400).send("Missing parameters");
+
+        if (this.recentReporters.has(reporter)) return res.status(200).send("Aspect reported");
 
         let tokenObject = await getToken(reporter);
 
@@ -28,6 +33,12 @@ class ReportAspectEndpoint {
 
         await insertAspect(giverUUID, receiverUUID, reporter);
         res.status(200).send("Aspect reported");
+
+        this.recentReporters.add(reporter);
+
+        setTimeout(() => {
+            this.recentReporters.delete(reporter);
+        }, 500);
     }
 }
 
