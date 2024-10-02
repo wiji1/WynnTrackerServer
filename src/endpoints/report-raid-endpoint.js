2061d149d67356d02c545f2dcd77844febd19c38
wiji <wijigaming.yt@gmail.com>
2024-10-02 11:17:42 -0700

    attempt to fix double raid reporting

--- src/endpoints/report-raid-endpoint.js
+++ src/endpoints/report-raid-endpoint.js
@@ -5,7 +5,7 @@ const {requestUUID} = require("../misc");
 
 class ReportRaidEndpoint {
     constructor() {
-        this.recentRaids = [];
+        this.recentRaids = new Map();
     }
 
     async call(req, res) {
@@ -14,43 +14,43 @@ class ReportRaidEndpoint {
 
         if (!raid || !token || !player1 || !player2 || !player3 || !player4 || !reporter || !seasonRating || !guildXP) return res.status(400).send("Missing parameters");
 
-        let tokenObject = await getToken(reporter);
-
-        if (!tokenObject || tokenObject.token !== token || !tokenObject.isAuthenticated()) return res.status(400).send("Invalid token");
-        if (!await isPlayerInGuild(reporter)) return res.status(400).send("Reporter is not in the guild");
-
-        let players = [player1, player2, player3, player4];
-
-        for (let i = 0; i < players.length; i++) {
-            let player = players[i];
-
-            let uuid = await getPlayerUUID(player);
-
-            if (!uuid) uuid = await requestUUID(player);
-            if (!uuid) return res.status(400).send("Invalid player: " + player);
-            if (this.checkForRecentRaid(uuid)) return res.status(200).send("Raid Reported");
-
-            players[i] = uuid;
+        const reportKey = `${player1}-${player2}--${player3}--${player4}`;
+        if (this.recentRaids.has(reportKey)) {
+            await this.recentRaids.get(reportKey);
+            return res.status(200).send("Raid reported");
         }
 
-        console.log("Reporting raid: ", raid, player1, player2, player3, player4, reporter, seasonRating, guildXP);
+        const reportPromise = (async () => {
+            let tokenObject = await getToken(reporter);
 
-        await insertRaid(raid, players[0], players[1], players[2], players[3], reporter, seasonRating, guildXP);
-        this.addRecentRaid(players);
-        res.status(200).send("Raid reported");
-    }
+            if (!tokenObject || tokenObject.token !== token || !tokenObject.isAuthenticated()) return res.status(400).send("Invalid token");
+            if (!await isPlayerInGuild(reporter)) return res.status(400).send("Reporter is not in the guild");
 
-    checkForRecentRaid(player) {
-        return this.recentRaids.includes(player);
-    }
+            let players = [player1, player2, player3, player4];
 
-    addRecentRaid(players) {
-        players.forEach(player => {
-            this.recentRaids.push(player);
+            for (let i = 0; i < players.length; i++) {
+                let player = players[i];
+
+                let uuid = await getPlayerUUID(player);
+
+                if (!uuid) uuid = await requestUUID(player);
+                if (!uuid) return res.status(400).send("Invalid player: " + player);
+
+                players[i] = uuid;
+            }
+
+            console.log("Reporting raid: ", raid, player1, player2, player3, player4, reporter, seasonRating, guildXP);
+
+            await insertRaid(raid, players[0], players[1], players[2], players[3], reporter, seasonRating, guildXP);
+            res.status(200).send("Raid reported");
+
             setTimeout(() => {
-                this.recentRaids = this.recentRaids.filter(p => p !== player);
-            }, 60000);
-        });
+                this.recentRaids.delete(reportKey);
+            }, 1000 * 60);
+        })();
+
+        this.recentRaids.set(reportKey, reportPromise);
+        await reportPromise;
     }
 }
 
